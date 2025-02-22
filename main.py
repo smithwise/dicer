ace3ed2854cf693aebf863923eed01eeffe13461
smithwise <smithwise.secure@gmail.com>
2025-02-22 00:32:39 -0600

    finished main.py version 1
    completed first draft of stage 1&2. Functional product.
    
    However before moving on to stage 3 I want to add the following functionality:
    - as a part of validity check, remove errant input and continue working with rest of input.
    - count 0dY as erroneous

--- main.py
+++ main.py
@@ -1,63 +1,45 @@
 import re
-from random import randint as dx
+import random
+#---------------------------> definitions
+def split_input(user_input, *delimiter): #split input into list
+    input_list = user_input.split(*delimiter)
+    return input_list  
 
-def split_input(user_input):
-  """
-  this splits input by whitespace, and indexes into a list of strings, then returns the list
-  :param user_input:
-  :return:
-  """
-  input_tuple = tuple(user_input.split())
-  return input_tuple #tuple to maintain input integrity
+def validate_input(input_list):
+    err = []
+    validity = True
+    for input_string in input_list:  # loops through each indexed input
+        if re.match(r"^\d+d(4|6|8|10|12|20|100)$", input_string): #matches xdy, where x is any int and y is one of the 7 die types
+            pass
+        else:
+            err.append(input_string) #logs errant input to list
+    if len(err) > 0:
+        validity = False
+    return validity, err  # returns boolean and list from function
 
-def validate_input(input_tuple):
-  """
-  this uses regex to validate proper formatting of input, for now 0dX is an acceptable input
-  :param input_tuple:
-  :return:
-  """
-  err = []
-  validity = True
-  for input_string in input_tuple: # loops through each indexed input
-    if re.match(r"^\d+d(4|6|8|10|12|20|100)$", input_string):
-        pass
-    else:
-        err.append(input_string)
-  if len(err) > 0:
-    validity = False
-  return validity, err #returns boolean and list from function
+def determine_value(qt, dSize):
+    value = 0
+    for iter in range(qt):
+        r = random.randint(1, dSize)
+        value += r
+    return value
 
-def roll_dice(input):
-    
-
-input_seed = input("Input dice to roll in format xdy (xdy ...) \ninput: ")
-    #erase string and remove comment mark for prod. Allows user input with prompt
-roll_tuple = split_input(input_seed)
-    #splits input by whitespace, returns a tuple
-valid, error_list = validate_input(roll_tuple) #checks list against regex
-    #valid shows if matches allowed regex, and if not delivers list of input that doesnt match
+# -------------------> main
+input_seed = input("Input dice to roll in format xdy (xdy ...) \ninput: ") #prompt
+results = {}
+str_list = split_input(input_seed) #generates list of input items
+valid, error_list = validate_input(str_list)  # checks list against regex
+    # valid shows if matches allowed regex, and if not delivers list of input that doesnt match
 if not valid:
     print(f'You put {input_seed}.\ninput {error_list} does not match regex')
 
 if valid:
-    #this only happens if there are no validation errors
-    input_string = ""
-    returned_value = 0
-    results = {} #k=input_string, v=returned_value
-    roll_matrix = []
-    roll_values = []
+    temp_list = []
+    int_list = []
+    for i in range(len(str_list)): #iter through list, strip 'd' and convert str to int
+        temp_list.append(split_input(str_list[i],'d'))
+        temp_list[i] = list(map(int, temp_list[i]))
+        int_list.append(determine_value(temp_list[i][0],temp_list[i][1]))
 
-    for i in range(len(roll_tuple)):
-        for str_in in roll_tuple: #iterate the tuple
-            results.update({str_in:0})
-            temp_list = str_in.split('d') #splits xdy into [x, y]
-            int_list = list(map(int, temp_list)) #converts str in list to int
-            roll_matrix.append(int_list) #appends new list to roll_matrix
-
-        for list in roll_matrix: #iterate through lists in matrix
-            value=0 #zeros value for each loop of the matrix
-            for i in range(int(list[0])): #range should be x
-                rand = dx(1, int(list[1])) #(1, y) gives range of y sided die
-                value += rand #sums the random numbers iterated
-            roll_values.append(value) #add value to the list
-        print(roll_values)
+    for n in range(len(str_list)):
+        results.update({str_list[n]:int_list[n]})
